b5412917e7d88a30ebb4561d2268e657b6876b0f
余家豪 YU KAHAO <124207407+zo-glass@users.noreply.github.com>
2026-01-01 00:00:00 -0300

    Modularize pagination data

--- src/app/discography/page.js
+++ src/app/discography/page.js
@@ -1,48 +1,22 @@
 import styles from './page.module.css'
 import { notFound } from 'next/navigation'
 
-import { getData } from '@/common/apiService'
-import { create, read, clear } from '@/common/redisCache'
-
 import GridWrapper from '@/components/layout/GridWrapper/GridWrapper'
 import MusicCard from '@/components/cards/MusicCard/MusicCard'
 import Pagination from '@/components/ui/Pagination/Pagination'
+
+import { fetchPaginatedData } from '@/common/dataFetcher'
 
 const ITEMS_PER_PAGE = (2) * 2
 
 export default async function Discography({ searchParams }) {
     const { page = 1 } = await searchParams
-    const offset = (page - 1) * ITEMS_PER_PAGE
-    let params = {}
-    let token = null
 
-    if (Number(page) === 1) {
-        params = { limit: ITEMS_PER_PAGE, offset: offset }
-    } else {
-        token = await read('discography', page)
-        if (token.token) {
-            params = { limit: ITEMS_PER_PAGE, pageToken: token.token }
-        } else {
-            params = { limit: ITEMS_PER_PAGE, offset: offset }
-        }
-    }
+    const { items, totalPages } = await fetchPaginatedData('discography', page, ITEMS_PER_PAGE, Number(process.env.REVALIDATE_TIME_LONG))
 
-    const res = await getData('discography', {
-        params: params,
-        revalidate: Number(process.env.REVALIDATE_TIME_LONG),
-        tags: ['discography']
-    })
+    items?.sort((a, b) => b.createdAt - a.createdAt)
 
-    if (!token?.hasNext && res?.nextPageToken) {
-        await create('discography', String(Number(page)+1), res?.nextPageToken)
-    }
-
-    const discographyItems = res?.items || []
-    discographyItems?.sort((a, b) => b.createdAt - a.createdAt)
-
-    const totalPages = Math.ceil(res?.pageInfo?.totalResults / ITEMS_PER_PAGE) || 1
-
-    if (discographyItems.length === 0) {
+    if (items.length === 0) {
         notFound()
     }
 
@@ -50,7 +24,7 @@ export default async function Discography({ searchParams }) {
         <>
             <h1>Discography</h1>
             <GridWrapper columns={2} mobileColumns={1}>
-                {discographyItems?.map((item, index) => (
+                {items?.map((item, index) => (
                     <MusicCard key={index} item={item} />
                 ))}
             </GridWrapper>
---
+++ src/common/dataFetcher.js
@@ -0,0 +1,36 @@
+import { create, read } from '@/common/redisCache'
+import { getData } from '@/common/apiService'
+
+export async function fetchPaginatedData(path, page, itemsPerPage, revalidate=0) {
+
+    const offset = (page - 1) * itemsPerPage
+    let params = {}
+    let token = null
+
+    if (Number(page) === 1) {
+        params = { limit: itemsPerPage, offset: offset }
+    } else {
+        token = await read(path, page)
+        if (token.token) {
+            params = { limit: itemsPerPage, pageToken: token.token }
+        } else {
+            params = { limit: itemsPerPage, offset: offset }
+        }
+    }
+
+    const res = await getData(path, {
+        params: params,
+        revalidate: revalidate,
+        tags: [path]
+    })
+
+    if (res?.nextPageToken) {
+        await create(path, String(Number(page) + 1), res?.nextPageToken)
+    }
+
+    const items = res?.items || []
+
+    const totalPages = Math.ceil(res?.pageInfo?.totalResults / itemsPerPage) || 1
+
+    return { items, totalPages }
+}

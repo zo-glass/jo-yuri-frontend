a44f21d810773ab76e60eee56f72ad52277432ad
余家豪 YU KAHAO <124207407+zo-glass@users.noreply.github.com>
2026-01-01 00:00:00 -0300

    Improve SEO

---
+++ src/app/video/LoadMore.js
@@ -0,0 +1,40 @@
+'use client'
+
+import { useState } from 'react'
+import { fetchMoreData } from '@/common/dataFetcher'
+
+import styles from './LoadMore.module.css'
+
+import GridWrapper from '@/components/layout/GridWrapper/GridWrapper'
+import VideoCard from '@/components/cards/VideoCard/VideoCard'
+import Button from '@/components/ui/Button/Button'
+
+export default function LoadMore({ initialItems, nextToken, size }) {
+    const [items, setItems] = useState(initialItems)
+    const [token, setToken] = useState(nextToken)
+    const [loading, setLoading] = useState(false)
+
+    const loadMore = async () => {
+        setLoading(true)
+        const { items, nextToken } = await fetchMoreData('video', token, size, Number(process.env.NEXT_PUBLIC_REVALIDATE_TIME_SORT))
+
+        items?.sort((a, b) => b.createdAt - a.createdAt)
+
+        setItems((prev) => [...prev, ...items])
+        setToken(nextToken)
+        setLoading(false)
+    }
+
+    return (
+        <>
+            <GridWrapper columns={2} mobileColumns={1}>
+                {items?.map((item, index) => (
+                    <VideoCard key={index} item={item} />
+                ))}
+            </GridWrapper>
+            {token && <div className={styles.loadMore}>
+                <Button onClick={loadMore}>{loading ? 'Loading...' : 'Load More'}</Button>
+            </div>}
+        </>
+    )
+}
--- src/app/video/page.js
+++ src/app/video/page.js
@@ -1,49 +1,18 @@
-'use client'
-
-import { useState, useEffect } from 'react'
-
 import styles from './page.module.css'
 
 import { notFound } from 'next/navigation'
 import { fetchMoreData } from '@/common/dataFetcher'
 
-import GridWrapper from '@/components/layout/GridWrapper/GridWrapper'
-import VideoCard from '@/components/cards/VideoCard/VideoCard'
-import Button from '@/components/ui/Button/Button'
+import LoadMore from './LoadMore'
 
 const ITEMS_PER_PAGE = (2) * 4
 
-export default function Video() {
-    const [items, setItems] = useState([])
-    const [token, setToken] = useState(null)
-    const [loading, setLoading] = useState(true)
+export default async function Video() {
+    const { items, nextToken } = await fetchMoreData('video', null, ITEMS_PER_PAGE + 1, Number(process.env.NEXT_PUBLIC_REVALIDATE_TIME_SORT))
 
-    useEffect(() => {
-        const init = async () => {
-            setLoading(true)
-            const { items, nextToken } = await fetchMoreData('video', null, ITEMS_PER_PAGE + 1, Number(process.env.NEXT_PUBLIC_REVALIDATE_TIME_SORT))
+    items?.sort((a, b) => b.createdAt - a.createdAt)
 
-            items?.sort((a, b) => b.createdAt - a.createdAt)
-
-            setItems(items)
-            setToken(nextToken)
-            setLoading(false)
-        }
-        init()
-    }, [])
-
-    const loadMore = async () => {
-        setLoading(true)
-        const { items, nextToken } = await fetchMoreData('video', token, ITEMS_PER_PAGE, Number(process.env.NEXT_PUBLIC_REVALIDATE_TIME_SORT))
-
-        items?.sort((a, b) => b.createdAt - a.createdAt)
-
-        setItems((prev) => [...prev, ...items])
-        setToken(nextToken)
-        setLoading(false)
-    }
-
-    if (!loading && items.length === 0) {
+    if (items.length === 0) {
         notFound()
     }
 
@@ -54,14 +23,7 @@ export default function Video() {
                 <div className={styles.video}>
                     <iframe src={`https://www.youtube.com/embed/${items[0]?.youtubeId}`} title={items[0]?.title} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
                 </div>
-                <GridWrapper columns={2} mobileColumns={1}>
-                    {items.slice(1).map((item, index) => (
-                        <VideoCard key={index} item={item} />
-                    ))}
-                </GridWrapper>
-                {token && <div className={styles.loadMore}>
-                    <Button onClick={loadMore}>{loading ? 'Loading...' : 'Load More'}</Button>
-                </div>}
+                <LoadMore initialItems={items?.slice(1)} nextToken = {nextToken} size = {ITEMS_PER_PAGE} />
             </div>
         </>
     )
